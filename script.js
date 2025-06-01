e950aa157ee13c7b6ce549a7821de32b0a524cdf
spin <tgvbnm7980@gmail.com>
2025-06-01 12:32:57 +0800

    練習08

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let 分數 = 85;
+let 分數 = 55;
 let 及格 = 60;
-if (分數 >= 80) {
-    console.log("考得很好!");
-}else if(分數 >= 及格){
-    console.log("恭喜你及格了!");
+let 出席率 = 0.9
+if (分數 >= 及格 && 出席率 >= 0.8) {
+    console.log("恭喜通過課程!");
+}else if(分數 >= 及格 || 出席率 >= 0.8){
+    console.log("通過，但需要補交作業");
 }else{
-    console.log("不及格，請繼續加油");
+    console.log("未通過課程");
 }
