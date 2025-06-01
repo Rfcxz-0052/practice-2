e5f07d86823de8c0237dc0a456eda5c8a1dd5d52
spin <tgvbnm7980@gmail.com>
2025-06-01 11:55:50 +0800

    練習07 if 條件式

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let 分數 = 70;
+let 分數 = 85;
 let 及格 = 60;
-if (分數 >= 及格) {
+if (分數 >= 80) {
+    console.log("考得很好!");
+}else if(分數 >= 及格){
     console.log("恭喜你及格了!");
 }else{
     console.log("不及格，請繼續加油");
