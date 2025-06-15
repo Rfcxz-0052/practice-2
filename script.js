8bf28025381fac86ffee3d0d6df73ea4e85cd07b
spin <tgvbnm7980@gmail.com>
2025-06-15 12:35:41 +0800

    實作練習08

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const 文字欄 = document.querySelector(".文字欄");
-const 清單 = document.querySelector(".清單")
+const 清單 = document.querySelector(".清單");
+const 按鈕 = document.querySelector(".按鈕");
 
 function 新任務() {
     if(文字欄.value === ""){
@@ -9,7 +10,9 @@ function 新任務() {
     任務.textContent = 文字欄.value;
     清單.append(任務);  //用 append (附加) 的語法 把新任務放到清單的底部
     文字欄.value = ""; //把文字欄清空，方便用戶輸入下一個任務
-}
+};
+
+按鈕.addEventListener("click", 新任務);
 
 文字欄.addEventListener("keyup", function(e){
     if(e.key === "Enter"){
