1f063f66fdc1affc6f065750442a6a34dd84e4d3
spin <tgvbnm7980@gmail.com>
2025-06-15 12:17:22 +0800

    實作練習06

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const 文字欄 = document.querySelector(".文字欄");
+const 清單 = document.querySelector(".清單")
+
+function 新任務() {
+    const 任務 = document.createElement("li");
+    任務.textContent = 文字欄.value;
+    清單.append(任務);  //用 append (附加) 的語法 把新任務放到清單的底部
+    文字欄.value = ""; //把文字欄清空，方便用戶輸入下一個任務
+}
 
 文字欄.addEventListener("keyup", function(e){
     if(e.key === "Enter"){
-        console.log(文字欄.value);
+        新任務();
     }
 });
 
